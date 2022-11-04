b99dd7b2fca6d4d66c410294b2d3b9dac9e75c23
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-04 23:05:52 +0000

    Getting the latest flight number and scheduling new launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,7 +1,7 @@
 const launches = require('./launches.mongo')
 const planets = require('./planets.mongo')
 
-let latestFlightNumber = 100
+const DEFAULT_FLIGHT_NUMBER = 100
 
 const launch = {
     flightNumber: 100,
@@ -21,6 +21,17 @@ function existsLaunchWithId(launchId) {
     return launches.has(launchId)
 }
 
+async function getLatestFlightNumber() {
+    const latestLaunch = await launches
+        .findOne()
+        .sort('-flightNumber')
+
+    if (!latestLaunch) {
+        return DEFAULT_FLIGHT_NUMBER
+    }
+    return latestLaunch.flightNumber
+}
+
 async function getAllLaunches() {
     return await launches.find({}, { '_id': 0, '__v': 0 })
 }
@@ -34,22 +45,24 @@ async function saveLaunch(launch) {
     if (!planet) {
         throw new Error('No matching planet found')
     }
-    await launches.updateOne({
+    await launches.findOneAndUpdate({
         flightNumber: launch.flightNumber,
     }, launch, { upsert: true })
 }
 
-function addNewLaunch(launch) {
-    latestFlightNumber++
-    launches.set(
-        latestFlightNumber,
-        Object.assign(launch, {
-            success: true,
-            upcoming: true,
-            customers: ['ZTM', 'NASA'],
-            flightNumber: latestFlightNumber,
-        }))
+async function scheduleNewLaunch(launch) {
+    const newFlightNumber = await getLatestFlightNumber() + 1
+
+    const newLaunch = Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customers: ['ZTM', 'NASA'],
+        flightNumber: newFlightNumber,
+    })
+
+    await saveLaunch(newLaunch)
 }
+
 
 function abortLaunchById(launchId) {
     const aborted = launches.get(launchId)
@@ -60,7 +73,7 @@ function abortLaunchById(launchId) {
 
 module.exports = {
     getAllLaunches,
-    addNewLaunch,
     existsLaunchWithId,
     abortLaunchById,
+    scheduleNewLaunch
 }
