a05949151b3fec3048e52ed1f35fef64d207811d
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-04 16:20:26 +0000

    saving a launch and listing all launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,4 @@
-// const launches = require('./launches.mongo')
-const launches = new Map()
+const launches = require('./launches.mongo')
 
 let latestFlightNumber = 100
 
@@ -9,19 +8,26 @@ const launch = {
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
     target: 'Kepler-442 b',
-    customer: ['ZTM', 'NASA'],
+    customers: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
 }
 
-launches.set(launch.flightNumber, launch)
+saveLaunch(launch)
+
 
 function existsLaunchWithId(launchId) {
     return launches.has(launchId)
 }
 
-function getAllLaunches() {
-    return Array.from(launches.values())
+async function getAllLaunches() {
+    return await launches.find({}, { '_id': 0, '__v': 0 })
+}
+
+async function saveLaunch(launch) {
+    await launches.updateOne({
+        flightNumber: launch.flightNumber,
+    }, launch, { upsert: true })
 }
 
 function addNewLaunch(launch) {
@@ -31,7 +37,7 @@ function addNewLaunch(launch) {
         Object.assign(launch, {
             success: true,
             upcoming: true,
-            customer: ['ZTM', 'NASA'],
+            customers: ['ZTM', 'NASA'],
             flightNumber: latestFlightNumber,
         }))
 }
