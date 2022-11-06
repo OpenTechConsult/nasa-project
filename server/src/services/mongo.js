5d4d526fa58423cad07b31e383a170494444deb8
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-06 05:52:12 +0000

    Updating tests for Mongoose

---
+++ server/src/services/mongo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const MONGO_URL = 'mongodb+srv://nasa-api:HctuYH5hHHxq9bsa@nasacluster.6ebmx1z.mongodb.net/nasa?retryWrites=true&w=majority'
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error()
+})
+
+async function mongoConnect() {
+    mongoose.connect(MONGO_URL)
+}
+
+async function mongoDisconnect() {
+    await mongoose.disconnect()
+}
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect,
+}
